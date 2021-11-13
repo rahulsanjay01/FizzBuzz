8782ca7832bb37ba20c7fc6e273f7942d3e1cdd0
rahulsanjay01 <90293567+rahulsanjay01@users.noreply.github.com>
2021-11-13 10:51:32 -0800

    Drafting the pseudocode of the site.js page using comments

--- js/site.js
+++ js/site.js
@@ -1,4 +1,21 @@
-//call Hello World
-function HelloWorld(){
-    alert("Hello World");
+function getValues()
+{
+    // 1.) Declared the variable "fizzValue" using "let" and assigned it to the value that is found in the HTML element with the id "fizzValue" using document.getElementById.
+
+    // 2.) Declared the variable "buzzValue" using "let" and assigned it to the value that is found in the HTML element with the id "buzzValue" using document.getElementById.
+
+    // 3.) Using parseInt() to convert "fizzValue" and "buzzValue" to an integer.
+    
+    // 4.) Testing if "fizzValue" and "buzzValue" are integers using an if-statement.
+
+    // Call generateNumbers()
+    // Using "let" to declare the variable "fbData" and setting it equal to the result of FizzBuzz(fizzValue, buzzValue) 
+
+    // Call displayNumbers()
+    // Calling the custom function "displayData()" and using "fbData" as the parameter
+        
+
+    // Alerting the user if "fizzValue" and "buzzValue" are not Numbers
+
+    // 5.)
 }
