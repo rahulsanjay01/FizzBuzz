8e6c1f3a461b893259d1815d2a909be26822468d
rahulsanjay01 <90293567+rahulsanjay01@users.noreply.github.com>
2021-11-15 14:43:10 -0800

    started adding functionality to the site.js page

--- js/site.js
+++ js/site.js
@@ -1,21 +1,52 @@
 function getValues()
 {
-    // 1.) Declared the variable "fizzValue" using "let" and assigned it to the value that is found in the HTML element with the id "fizzValue" using document.getElementById.
+    //- 1.) Declared the variable "fizzValue" using "let" and assigned it to the value that is found in the HTML element with the id "fizzValue" using document.getElementById.
+    let fizzValue = document.getElementById("fizzValue").value;
 
-    // 2.) Declared the variable "buzzValue" using "let" and assigned it to the value that is found in the HTML element with the id "buzzValue" using document.getElementById.
+    //- 2.) Declared the variable "buzzValue" using "let" and assigned it to the value that is found in the HTML element with the id "buzzValue" using document.getElementById.
+    let buzzValue = document.getElementById("buzzValue").value;
 
-    // 3.) Using parseInt() to convert "fizzValue" and "buzzValue" to an integer.
-    
-    // 4.) Testing if "fizzValue" and "buzzValue" are integers using an if-statement.
+    //- 3.) Using parseInt() to convert "fizzValue" and "buzzValue" to an integer.
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
 
-    // Call generateNumbers()
-    // Using "let" to declare the variable "fbData" and setting it equal to the result of FizzBuzz(fizzValue, buzzValue). 
-
-    // Call displayNumbers()
-    // Calling the custom function "displayData()" and using "fbData" as the parameter.
-        
-
-    // Alerting the user if "fizzValue" and "buzzValue" are not Numbers.
-
-    // 5.)
+    //- 4.) Testing if "fizzValue" and "buzzValue" are integers using an if-statement.
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+        //call fizzBuzz
+        let fbArray = fizzBuzz(fizzValue, buzzValue);
+        //call displayData and write the values to the screen
+        displayData(fbArray);
+        //Alerting the user if "fizzValue" and "buzzValue" are not Numbers.
+    }else{
+        alert("You must enter a integer")
+    }
 }
+
+//do fizzBuzz
+function FizzBuzz(fizzValue, buzzValue)
+{
+    let returnArray = [];
+
+    //loop from 1 to 100
+    for (let i = 1; i <= 100; i++) {
+        //checking to see if the number is divisible by 3, 5, or both
+        //pushing fizz, buzz, or fizzBuzz into the array if divisible by 3, 5, or both
+        if (i % fizzValue == 0 && i % buzzValue == 0){
+            returnArray.push('FizzBuzz');
+        } else if (i % fizzValue == 0) {
+            returnArray.push('Fizz');
+        } else if (i % buzzValue == 0) {
+            returnArray.push('Buzz');
+        } else {
+            returnArray.push(i);
+        }
+    }
+    returnArray;
+
+}
+//loop over the array and create a tablerow for each item.
+function displayData(fbArray){
+
+}
+
+
