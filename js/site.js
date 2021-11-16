7e2a0c9194073ee86a1fd8d52170dbae5c26c65e
rahulsanjay01 <90293567+rahulsanjay01@users.noreply.github.com>
2021-11-15 22:00:07 -0800

    Copleted the app.html page

--- js/site.js
+++ js/site.js
@@ -62,11 +62,21 @@ function displayData(fbArray){
 
         //grab use the to put into array
         let rowCols = tableRow.querySelectorAll("td");
+        
+        rowCols[0].classList.add(fbArray[index]);
         rowCols[0].textContent = fbArray[index];
-        rowCols[1].textContent = fbArray[index+1];
-        rowCols[2].textContent = fbArray[index+2];
-        rowCols[3].textContent = fbArray[index+3];
-        rowCols[4].textContent = fbArray[index+4];
+        
+        rowCols[1].classList.add(fbArray[index + 1]);
+        rowCols[1].textContent = fbArray[index + 1];
+        
+        rowCols[2].classList.add(fbArray[index + 2]);
+        rowCols[2].textContent = fbArray[index + 2];
+        
+        rowCols[3].classList.add(fbArray[index + 3]);
+        rowCols[3].textContent = fbArray[index + 3];
+        
+        rowCols[4].classList.add(fbArray[index + 4]);
+        rowCols[4].textContent = fbArray[index + 4];
 
         tableBody.appendChild(tableRow);
     }
